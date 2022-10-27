5e74345c4ca2d97e7f4c76d74f2b9999deab31dd
Nithin Gopal <nithingopal2015@gmail.com>
2022-10-27 22:16:47 +0530

    add search input basic

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { BiSearch } from 'react-icons/bi'
 
 const Header = () => {
   return (
@@ -15,14 +15,10 @@ const Header = () => {
                 position="obsolute" 
             />
         </div>
-        {/* middle-search bar  */}
-        <div>
-            <input type="text" placeholder='Start your search' />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
-  <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z" clipRule="evenodd" />
-</svg>
-
+        {/* middle-search */}
+        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+            <input type="text" placeholder='Start your search' className='flex-grow pl-2 bg-transparen outline-none text-sm text-gray-600 placeholder-gray-400' />
+            <BiSearch className='hidden md:inline-flex h-8 w-8 p-2 bg-red-400 rounded-full text-white cursor-pointer md:mx-2' size={12}/>
         </div>
         {/* right */}
         <div></div>
