444825d65c55a8d2dd02fe45705c72c4209bdd09
Nithin Gopal <nithingopal2015@gmail.com>
2022-10-28 19:52:44 +0530

    banner typo fix

--- components/Banner.js
+++ components/Banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
         <div className='absolute top-1/2 w-full text-center'>
             <p className='text-sm sm:text-lg font-semibold'>Not sure where to go? Perfect.</p>
             <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 ease-in-out'>
-                I'm Flexible
+                Im Flexible
             </button>
         </div>
     </div>
